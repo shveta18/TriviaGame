09caf85197a1b17dbfa896095277d4ac55ea5624
Shveta Kumar <sk@shveta.ca>
2020-08-26 21:34:41 -0400

    added quiz questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,31 @@
+
+
+//Quiz
+var quiz = [
+    {
+      question: "Who directed LOTR?",
+      answers: ["Peter Jackson", "Steven Speilberg.", "Quentin Tarantino", "Guillermo Del Torro"],
+      correctAnswer: "Peter Jackson"
+    },
+    {
+        question: "Boromir died saving who?",
+        answers: ["Hobbits", "Elrond", "The ring", "Galadriel"],
+        correctAnswer: "Hobbits"
+      },
+      {
+        question: "Aragorn was the descendent of which race",
+        answers: ["Dunedein", "Elves", "Hobbits", "Dwarves"],
+        correctAnswer: "Dunedein"
+      },
+      {
+        question: "Which dwarf family did Gimli belong to? ",
+        answers: ["Dunedein", "Elves", "Gloin", "Orin"],
+        correctAnswer: "Gloin"
+      },
+      {
+        question: "What is the superpower of elves?",
+        answers: ["Live forever", "Can see great distances", "Can hear whispers", "All of these"],
+        correctAnswer: "All of these"
+      }
+
+]
